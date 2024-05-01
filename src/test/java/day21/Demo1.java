da4559fd84cf66478183721b88de812affd15dd9
delsonroy <delson.geo@gmail.com>
2024-05-01 02:00:18 -0400

    Added Extent Report

--- src/test/java/day21/Demo1.java
+++ src/test/java/day21/Demo1.java
@@ -11,18 +11,33 @@ import org.testng.Reporter;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.observer.ExtentObserver;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class Demo1 {
 
 	@Parameters({"gridURL", "appURL"})
 	@Test
 	public void testA(String gridURL, String appURL) throws Exception
 	{
+		
+		ExtentReports report = new ExtentReports();
+		ExtentSparkReporter spark= new ExtentSparkReporter("report/Spark.html");
+		report.attachReporter(spark);
+		
+		ExtentTest extentTest = report.createTest("testaA");
+		
+		extentTest.info("testA of Demo1 of 21"); 
         Reporter.log("Demo1 of testA...", true);    
         WebDriver driver = new RemoteWebDriver(new URL(gridURL), new EdgeOptions());
         driver.get(appURL);
-        System.out.println(driver.getTitle());
+        
+        extentTest.info(driver.getTitle());
         Thread.sleep(2000);
         driver.quit();
+        report.flush();
 		
 	}
 
